2bacd2ee6a82ece0471ca85fa0800ea1478b3f4e
Airiinnn <alex.liswandy@hotmail.com>
2020-02-08 22:25:03 +0800

    Basic functionalities

--- app.py
+++ app.py
@@ -5,21 +5,24 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("index.html")
+        return render_template("index.html", valid=True)
     else:
         scale = request.form.get("scale")
         temp = request.form.get("temp")
+        if not temp.isdigit():
+            return render_template("index.html", valid=False)
+
         temp = float(temp)
         
-        if scale == "f":
+        if scale == "F":
             temp = round(((temp - 32) * 5 / 9), 1)
             
         if temp >= 38.0:
-            risk = "high"
+            risk = "High"
         else:
-            risk = "low"
+            risk = "Low"
         
-        return render_template("index.html", temp=temp, scale=scale, risk=risk)
+        return render_template("index.html", valid=True, temp=temp, scale=scale, risk=risk)
         
 if __name__ == "__main__":
     app.run(debug=True)
